f78e94d72b326ecdaeee5cb9fc11bd1a20d57ebd
NanaKhay18 <gyedusamuelk22@gmail.com>
2026-05-29 21:42:20 +0000

    Add files via upload

---
+++ GSA.py
@@ -0,0 +1,30 @@
+# File: GSA.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Gyedu Samuel Amponsah
+# STUDENT ID:2425403486
+# Email: 2425403486@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Gyedu Samuel Amponsah, this programming assignment is my own work
+# and I have not provided this code to another student.
+
+print()
+
+print("...GSA")
+
+print()
+
+print("....GGGGGGGGGG...........SSSSSSSSSS............AAAA.........")
+print("...GGGGGGGGGGGG.........SSSSSSSSSSS...........AA..AA........")
+print("...GGG.................SSSS..................AA....AA.......")
+print("...GGG.................SSSS.................AA......AA......")
+print("...GGG...GGGGGG..........SSSSSS............AAAAAAAAAAAA.....")
+print("...GGG...GGGGGGG...........SSSSSS..........AAAAAAAAAAAA.....")
+print("...GGG......GGGG...............SSSS........AAA......AAA.....")
+print("...GGG......GGGG...............SSSS........AAA......AAA.....")
+print("...GGGGGGGGGGGGG..**....SSSSSSSSSS....**...AAA......AAA...**")
+print("....GGGGGGGGGGG...**....SSSSSSSSS.....**...AAA......AAA...**")
+
+print()
